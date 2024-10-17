e6dc69eb292c5f8723faa52427e15f8f76da506c
leleintrona6 <6lelin@gmail.com>
2024-10-17 15:22:41 +0200

    ParticleType done

---
+++ src/ParticleType.cpp
@@ -0,0 +1,44 @@
+#include "ParticleType.hpp"
+#include <stdexcept>
+#include <iostream>
+namespace kape
+{
+    ParticleType::ParticleType(const char *name, double mass, int charge)
+        : fName{name}, fMass{mass}, fCharge{charge}
+    {
+        if (mass <= 0.)
+        {
+            throw std::invalid_argument{"mass can't be negative or null"};
+        }
+
+        if (name == nullptr)
+        {
+            throw std::invalid_argument{"name can't point to nullptr"};
+        }
+    }
+
+    ParticleType::ParticleType()
+        : ParticleType("", 1, 0) {}
+
+    const char *ParticleType::GetName() const
+    {
+        return fName;
+    }
+
+    double ParticleType::GetMass() const
+    {
+        return fMass;
+    }
+
+    int ParticleType::GetCharge() const
+    {
+        return fCharge;
+    }
+
+    void ParticleType::Print() const
+    {
+        std::cout << "Name:\t" << fName << '\n';
+        std::cout << "Mass:\t" << fMass << '\n';
+        std::cout << "Charge:\t" << fCharge << '\n';
+    }
+}
--- src/ParticleType.hpp
+++ src/ParticleType.hpp
@@ -1,18 +1,25 @@
 #ifndef PARTICLE_TYPE_HPP
 #define PARTICLE_TYPE_HPP
 
-class ParticleType
+namespace kape
 {
-public:
-    ParticleType();
-    ParticleType(const char * name, double mass, int charge);
+    class ParticleType
+    {
+    public:
+        ParticleType();
+        ParticleType(const char *name, double mass, int charge);
 
-    const char* GetName() const;
-    double GetMass() const;
-    int GetCharge() const;
-private:
-    const char *fName;
-    const double fMass;
-    const int fCharge;
-};
+        const char *GetName() const;
+        double GetMass() const;
+        int GetCharge() const;
+
+        void Print() const;
+
+    private:
+        const char *fName;
+        const double fMass;
+        const int fCharge;
+    };
+}
+
 #endif
