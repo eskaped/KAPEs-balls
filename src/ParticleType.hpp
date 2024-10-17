ab00309b84939a2903a500c2852efd4e30653e08
samu444 <samuele.bellini@gmail.com>
2024-10-17 15:03:07 +0200

    changing pc

--- src/ParticleType.hpp
+++ src/ParticleType.hpp
@@ -1,4 +1,18 @@
 #ifndef PARTICLE_TYPE_HPP
 #define PARTICLE_TYPE_HPP
 
+class ParticleType
+{
+public:
+    ParticleType();
+    ParticleType(const char * name, double mass, int charge);
+
+    const char* GetName() const;
+    double GetMass() const;
+    int GetCharge() const;
+private:
+    const char *fName;
+    const double fMass;
+    const int fCharge;
+};
 #endif
